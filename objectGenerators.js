2e40c469aab5db11737b8e8678127e7e21e4f56a
MasterCoder1323 <121037342+MasterCoder1323@users.noreply.github.com>
2023-01-14 08:50:11 -0500

    Update objectGenerators.js

--- objectGenerators.js
+++ objectGenerators.js
@@ -1,32 +1,32 @@
 function album(songs,photos,audioId,viewerId){
-    this.songs:songs,
-    this.songIndex:0,
-    this.currentSong:this.songs[this.songIndex],
-    this.photos:photos,
-    this.currentPhoto:this.photos[this.songIndex],
-    this.audio: document.getElementById(audioId),
-    this.viewer: document.getElementById(viewerId),
-    this.play: function(){
+    this.songs=songs,
+    this.songIndex=0,
+    this.currentSong=this.songs[this.songIndex],
+    this.photos=photos,
+    this.currentPhoto=this.photos[this.songIndex],
+    this.audio= document.getElementById(audioId),
+    this.viewer= document.getElementById(viewerId),
+    this.play= function(){
         this.audio.src = this.currentSong;
         this.audio.play();
     }
-    this.pause: function(){
+    this.pause= function(){
         this.audio.pause();
     }
-    this.setSong: function(index){
+    this.setSong= function(index){
         this.songIndex = index;
-        this.currentPhoto:this.photos[this.songIndex];
-        this.currentSong:this.songs[this.songIndex];
+        this.currentPhoto=this.photos[this.songIndex];
+        this.currentSong=this.songs[this.songIndex];
     }
-    this.next: function(){
+    this.next= function(){
         this.songIndex++
-        this.currentPhoto:this.photos[this.songIndex];
-        this.currentSong:this.songs[this.songIndex];
+        this.currentPhoto=this.photos[this.songIndex];
+        this.currentSong=this.songs[this.songIndex];
     }
-    this.previouse: function(){
+    this.previouse= function(){
         this.songIndex--
-        this.currentPhoto:this.photos[this.songIndex];
-        this.currentSong:this.songs[this.songIndex];
+        this.currentPhoto=this.photos[this.songIndex];
+        this.currentSong=this.songs[this.songIndex];
     }
     
 };
